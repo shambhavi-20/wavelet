45e8cbc3439aacf72a22c1b224974ba9497c91c1
shambhavi-20 <shmittal@ucsd.edu>
2023-01-26 23:41:27 -0800

    String server

---
+++ StringServer.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> data = new ArrayList<String>();
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Shambhavi's search engine");
+        } 
+        
+        else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s") ) {
+                    data.add("\n"+parameters[1]);
+                    return data.toString();
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
